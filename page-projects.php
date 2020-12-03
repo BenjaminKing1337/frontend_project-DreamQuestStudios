b280eaa8595cd895ac80c0c1754a136d8b7e0561
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2020-12-03 20:12:28 +0100

    slide

--- page-projects.php
+++ page-projects.php
@@ -30,12 +30,70 @@ get_header(); ?>
             <div id="xElementOne">
                 <div id="xElOneText">
                     <h2><?php the_field('1st_header'); ?></h2>
-                    <p><?php the_field('1st_paragraph'); ?></p>
+                    <!-- <p><?php the_field('1st_paragraph'); ?></p> -->
                 </div>
-                <?php if (get_field('1st_img')) : ?>
+<!--                 <?php if (get_field('1st_img')) : ?>
                     <img src="<?php the_field('1st_img'); ?>" />
-                <?php endif; ?>
+                <?php endif; ?> -->
             </div>
+            <div class="slideshow-container">
+
+        <div class="mySlides fade">
+        <div class="numbertext">1 / 3</div>
+        <img src="<?php the_field('1st_img'); ?>" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="<?php the_field('2nd_img'); ?>" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="<?php the_field('3rd_img'); ?>" style="width:100%">
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+        </div>
+        <br>
+
+        <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span> 
+            <span class="dot" onclick="currentSlide(2)"></span> 
+            <span class="dot" onclick="currentSlide(3)"></span> 
+        </div>
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
 
 
         </article>
